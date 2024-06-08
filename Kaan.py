719c89b678f5ad34e4fadcb85963d361d632f366
Kacosk <kaancoskun3442@gmail.com>
2024-06-08 13:28:07 +0300

    Update Kaan.py

--- Kaan.py
+++ Kaan.py
@@ -1,12 +1,11 @@
 
 import random
-
+#koyulabilen karakterler
 characters = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-
+#uzunluk
 password_length = int(input("Parolanın uzunluğunu girin:"))
-
+#şifre
 password = ""
-
 for _ in range(password_length):
     password += random.choice(characters)
 
